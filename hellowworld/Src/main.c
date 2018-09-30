b8a841c87288d0c38165808b22789da511a10b0f
Natalia <42980988+MaltsevaNata@users.noreply.github.com>
2018-09-30 15:56:24 +0300

    changes

--- hellowworld/Src/main.c
+++ hellowworld/Src/main.c
@@ -108,9 +108,9 @@ int main(void)
 
   /* USER CODE BEGIN 3 */
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
-		HAL_Delay(500);
+		HAL_Delay(700);
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
-		HAL_Delay(500);
+		HAL_Delay(900);
   }
   /* USER CODE END 3 */
 
